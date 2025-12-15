ff9f912d5d761bd6ac06654cf561fc7b8b86465a
notguigao-afk <notguigao@gmail.com>
2025-12-15 16:56:36 +0800

    fix bugs

--- samples/templatetags/sample_tags.py
+++ samples/templatetags/sample_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import get_language
 
 register = template.Library()
 
@@ -40,4 +41,32 @@ def is_language(current_lang, check_lang):
     current_lower = str(current_lang).lower().replace('_', '-')
     check_lower = str(check_lang).lower().replace('_', '-')
     return current_lower == check_lower or check_lower in current_lower
+
+
+@register.simple_tag
+def localized_date(date_obj):
+    """Return a localized date string based on current language"""
+    if date_obj is None:
+        return ''
+    
+    lang = get_language() or 'en'
+    lang_lower = lang.lower()
+    
+    # Day of week mappings
+    weekdays_en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    weekdays_zh = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+    
+    weekday_idx = date_obj.weekday()
+    
+    if 'zh' in lang_lower:
+        # Chinese format: 2024年12月15日 星期一
+        day = date_obj.day
+        month = date_obj.month
+        year = date_obj.year
+        weekday = weekdays_zh[weekday_idx]
+        return f'{year}年{month}月{day}日 {weekday}'
+    else:
+        # English format: Monday, December 15, 2024
+        weekday = weekdays_en[weekday_idx]
+        return date_obj.strftime(f'{weekday}, %B %d, %Y')
 
